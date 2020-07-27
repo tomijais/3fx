88af98d24f3a26f56d7f76d5532156bb4929c85d
Tomas <tomijais@gmail.com>
2020-07-27 05:21:09 -0300

    form php completo

--- enviar.php
+++ enviar.php
@@ -3,18 +3,20 @@
 // Llamando a los campos
 $nombre = $_POST['name'];
 $correo = $_POST['email'];
-$asunto = $_POST['subject'];
+$subject = $_POST['subject'];
 $mensaje = $_POST['message'];
 
 // Datos para el correo
 $destinatario = "tomijais@gmail.com";
+$asunto = "Contacto desde nuestra web";
 
-$correo = "De: $nombre \n";
-$correo .= "Correo: $correo \n";
-$correo .= "Asunto: $asunto \n";
-$correo .= "Mensaje: $mensaje";
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Asunto: $subject \n";
+$carta .= "Mensaje: $mensaje";
 
 // Enviando Mensaje
-mail($destinatario, $asunto, $correo);
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
 
 ?>
